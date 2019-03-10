482cb6160e3b0cb7339be7bc7118d835e71bedd3
Alastair Innes <2317070i@student.gla.ac.uk>
2019-03-10 13:26:50 +0000

    Added dog model and made migrations

--- golden_leash/models.py
+++ golden_leash/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Dog(models.Model):
+
+    name = models.CharField(max_length=128)
+    age = models.IntegerField(default=0)
+    breed = models.CharField(max_length=128)
+    image = models.ImageField(upload_to="pictures/%Y/%m%d/", max_length=255)
+    slug = models.SlugField(unique=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Dog, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+    
