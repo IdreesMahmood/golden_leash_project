755388d30a361c46f3579052bf7b95f8d840b8ac
Gavin Mooney <2324864m@student.gla.ac.uk>
2019-03-18 17:49:13 +0000

    Added static files directory, setup template inheritance and basic css

--- golden_leash_project/settings.py
+++ golden_leash_project/settings.py
@@ -127,8 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [STATIC_DIR, ]
 
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
-
