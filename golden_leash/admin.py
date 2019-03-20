27913124cb2cd388228b4adfc9364f81107e08a7
Gavin Mooney <2324864m@student.gla.ac.uk>
2019-03-20 16:39:15 +0000

    Updated template titles/headers to inherit from base. Added foreign key to dog model

--- golden_leash/admin.py
+++ golden_leash/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from golden_leash.models import UserProfile
+from golden_leash.models import Dog
 
 # Register your models here
 
 
 
 admin.site.register(UserProfile)
+admin.site.register(Dog)
--- golden_leash/models.py
+++ golden_leash/models.py
@@ -2,21 +2,6 @@ from django.db import models
 from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 
-class Dog(models.Model):
-
-    name = models.CharField(max_length=128)
-    age = models.IntegerField(default=0)
-    breed = models.CharField(max_length=128)
-    image = models.ImageField(upload_to="pictures/%Y/%m%d/", max_length=255)
-    slug = models.SlugField(unique=True)
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(Dog, self).save(*args, **kwargs)
-
-    def __str__(self):
-        return self.name
-
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     is_owner = models.BooleanField(default=False)
@@ -24,3 +9,20 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Dog(models.Model):
+
+    name = models.CharField(max_length=128)
+    age = models.IntegerField(default=0)
+    breed = models.CharField(max_length=128)
+    image = models.ImageField(upload_to="pictures/%Y/%m%d/", max_length=255)
+    slug = models.SlugField(unique=True)
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Dog, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
