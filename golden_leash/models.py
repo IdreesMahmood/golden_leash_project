54f231644f708dd6a28fe287cf583f5513a85415
Idrees Mahmood <2324918m@student.gla.ac.uk>
2019-03-20 21:41:24 +0000

    fixed foreign key issue

--- golden_leash/models.py
+++ golden_leash/models.py
@@ -24,7 +24,7 @@ class Dog(models.Model):
     breed = models.CharField(max_length=128)
     image = models.ImageField(upload_to="pictures/%Y/%m%d/", max_length=255)
     slug = models.SlugField(unique=True)
-    owner = models.ForeignKey('UserProfile', on_delete=models.CASCADE, default=UserProfile.objects.first())
+    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE, default=UserProfile.objects.first())
 
 
 
