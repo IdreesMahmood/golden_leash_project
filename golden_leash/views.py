7a49477aba5c250a2dfce0580bfc694e8a583bef
Idrees Mahmood <2324918m@student.gla.ac.uk>
2019-03-21 11:59:44 +0000

    changed login page

--- golden_leash/views.py
+++ golden_leash/views.py
@@ -82,7 +82,7 @@ def register(request):
 
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
-
+            
             profile.save()
             registered = True
         else:
